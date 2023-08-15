2e48136d7405f6eaec5bf4168de8b6d38656c17d
Nadya Tyandra <nadyatyandra05@gmail.com>
2023-08-15 11:16:19 +0700

    Initial Commit

---
+++ MC3-kasih/MC3_kasihApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC3_kasihApp.swift
+//  MC3-kasih
+//
+//  Created by Nadya Tyandra on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MC3_kasihApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
