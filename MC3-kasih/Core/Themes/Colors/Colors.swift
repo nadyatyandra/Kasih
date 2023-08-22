8fcfd528f1268cd110e0ee4d0d9fd0b8ff04d040
Nadya Tyandra <nadyatyandra05@gmail.com>
2023-08-22 12:00:06 +0700

    implement mvvm architecture + colors + themes

---
+++ MC3-kasih/Core/Themes/Colors/Colors.swift
@@ -0,0 +1,27 @@
+//
+//  AppColor.swift
+//  MC3-kasih
+//
+//  Created by Nadya Tyandra on 22/08/23.
+//
+
+import SwiftUI
+
+struct Colors {
+    static let white = Color("white")
+    static let black = Color("black")
+
+    static let pp100 = Color("pp-100")
+    static let pp200 = Color("pp-200")
+    static let pp300 = Color("pp-300")
+    static let pp400 = Color("pp-400")
+    static let pp500 = Color("pp-500")
+
+    static let ab100 = Color("ab-100")
+    static let ab200 = Color("ab-200")
+    static let ab300 = Color("ab-300")
+    static let ab400 = Color("ab-400")
+    static let ab500 = Color("ab-500")
+
+    private init() {}
+}
---
+++ MC3-kasih/Core/Themes/Typography/Fonts.swift
@@ -0,0 +1,62 @@
+//
+//  Fonts.swift
+//  MC3-kasih
+//
+//  Created by Nadya Tyandra on 22/08/23.
+//
+
+import SwiftUI
+
+enum TypographyStyle {
+    case title
+    case heading1Reg
+    case heading1
+    case heading2Reg
+    case heading2
+    case largeReg
+    case large
+    case baseBold
+    case base
+    case smallBold
+    case small
+    
+    var defaultColor: Color {
+        return Colors.black
+    }
+    
+    var size: CGFloat {
+        switch self {
+        case .title: return 39
+        case .heading1, .heading1Reg: return 31
+        case .heading2, .heading2Reg: return 25
+        case .large, .largeReg: return 20
+        case .base, .baseBold: return 16
+        case .small, .smallBold: return 13
+        }
+    }
+    
+    var weight: Font.Weight {
+        switch self {
+        case .title, .heading2, .baseBold, .smallBold: return .bold
+        case .heading1, .large: return .semibold
+        case .heading1Reg, .heading2Reg, .largeReg, .base, .small: return .regular
+        }
+    }
+}
+
+struct BaseTypography: ViewModifier {
+    let type: TypographyStyle
+    
+    func body(content: Content) -> some View {
+        content
+            .fontWeight(type.weight)
+            .font(.system(size: type.size))
+            .foregroundColor(type.defaultColor)
+    }
+}
+
+extension View {
+    func typography(_ type: TypographyStyle) -> some View {
+        self.modifier(BaseTypography(type: type))
+    }
+}
